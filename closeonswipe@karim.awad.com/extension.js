5bbf2130213eb3d8caec1dd81a804b9672e73b13
iv65ojeq <karim.g.awad@fau.de>
2018-02-07 12:54:41 +0100

    initial code commit

---
+++ closeonswipe@karim.awad.com/extension.js
@@ -0,0 +1,21 @@
+const St = imports.gi.St;
+const EdgeDragAction = imports.ui.edgeDragAction;
+const Shell = imports.gi.Shell;
+const Lang = imports.lang;
+
+let gesture = new EdgeDragAction.EdgeDragAction(St.Side.TOP, Shell.ActionMode.NORMAL);
+
+function init() {}
+
+function enable() {
+    gesture.connect('activated', Lang.bind(this, function() {
+       	// close it.
+	global.display.focus_window.delete(global.get_current_time());
+    }));
+
+    global.stage.add_action(gesture);
+}
+
+function disable() {
+    global.stage.remove_action(gesture);
+}
